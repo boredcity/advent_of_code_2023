b6544b3c11114aa1aea5124a278257f8733e57ad
Georgii Perepechko <georgiiperepechko@gmail.com>
2023-12-02 18:49:31 +0000

    setup + day 1

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using AdventOfCode;
+namespace Main {
+    class Program {
+        static void Main() {
+            var day1Result1 = Day1.solve1();
+            var day1Result2 = Day1.solve2();
+            Console.WriteLine($"Results:");
+            Console.WriteLine($"Day 1: {day1Result1} / {day1Result2}");
+        }
+    }
+}
---
+++ day01-csharp/Day1.cs
@@ -0,0 +1,85 @@
+namespace AdventOfCode {
+
+    class Day1 {
+
+        public static int solve1() {
+            var lines = File.ReadLines("./inputs/day1.txt");
+            var sum = 0;
+            foreach (var line in lines) {
+                char? first = null;
+                char? last = null;
+                foreach (var lineChar in line) {
+                    if (char.IsDigit(lineChar)) {
+                        last = lineChar;
+                        if (!first.HasValue) {
+                            first = lineChar;
+                        }
+                    }
+                }
+                var result = $"{first}{last}";
+                sum += int.Parse(result);
+            }
+            return sum;
+        }
+
+        public static int solve2() {
+            var lines = File.ReadLines("./inputs/day1.txt");
+            var sum = 0;
+            foreach (var line in lines) {
+                char? first = null;
+                char? last = null;
+                var wordToCurrentMatch = new Dictionary<string, string>();
+                foreach (var kvp in wordToDigit) {
+                    wordToCurrentMatch[kvp.Key] = "";
+                }
+
+                foreach (var lineChar in line) {
+                    char? digit = null;
+                    if (char.IsDigit(lineChar)) {
+                        digit = lineChar;
+                    } else {
+                        foreach (var kvp in wordToCurrentMatch) {
+                            var currentMatchLength = kvp.Value.Length;
+                            var expectedChar = kvp.Key[currentMatchLength];
+                            var lastRequired = currentMatchLength == kvp.Key.Length - 1;
+                            if (lineChar == expectedChar) {
+                                if (lastRequired) {
+                                    digit = wordToDigit[kvp.Key];
+                                } else {
+                                    wordToCurrentMatch[kvp.Key] += lineChar;
+                                }
+                            } else {
+                                wordToCurrentMatch[kvp.Key] = kvp.Key.First() == lineChar ? lineChar.ToString() : "";
+                            }
+                        }
+                    }
+
+                    if (digit.HasValue) {
+                        if (!first.HasValue) {
+                            first = digit;
+                        }
+                        last = digit;
+                    }
+                }
+
+                var result = $"{first}{last}";
+                sum += int.Parse(result);
+            }
+            return sum;
+        }
+
+        private static readonly Dictionary<string, char> wordToDigit = new()
+        {
+            { "one", '1' },
+            { "two", '2' },
+            { "three", '3' },
+            { "four", '4' },
+            { "five", '5' },
+            { "six", '6' },
+            { "seven", '7' },
+            { "eight", '8' },
+            { "nine", '9' }
+        };
+    }
+}
+
