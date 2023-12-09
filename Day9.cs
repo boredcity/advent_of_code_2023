c059b74add33ef26604000dbd158daa32a4a0a4b
Georgii Perepechko <g.perepechko@thesignalgroup.com>
2023-12-09 22:24:36 +0200

    day 9 compact

--- Day9.cs
+++ Day9.cs
@@ -1,58 +1,15 @@
 namespace AdventOfCode {
     class Day9 : IDay {
+        public int calculate(List<int> list) => list.Exists(x => x != 0)
+            ? list.Last() + calculate(list.Zip(list.Skip(1), (el1, el2) => el2 - el1).ToList())
+            : 0;
 
-        public string solve1() {
-            var lines = File.ReadLines("./inputs/day9.txt");
-            List<int> results = new() {};
-            
-            foreach (var line in lines) {
-                var lineNumbers = line.Split(' ').Select(int.Parse).ToList();
-                List<List<int>> iterations = new() { lineNumbers };
-                while (true) {
-                    var differences = new List<int>() {};
-                    var onlySame = true;
-                    var lastIteration = iterations.Last();
-                    for (var i = 1; i < lastIteration.Count; i++) {
-                        var dif = lastIteration[i] - lastIteration[i - 1];
-                        if (lastIteration[i] != lastIteration[i - 1]) onlySame = false;
-                        differences.Add(dif);
-                    }
-                    iterations.Add(differences);
-                    if (onlySame) break;
-                }
+        public string solve1() => File.ReadLines("./inputs/day9.txt").Select(line => 
+                calculate(line.Split(' ').Select(int.Parse).ToList())
+            ).Sum().ToString();
 
-                iterations.Reverse();
-                results.Add(iterations.Aggregate(0, (acc, iteration) => iteration.Last() + acc));
-
-            }
-            return results.Sum().ToString();
-        }
-
-        public string solve2() {
-            var lines = File.ReadLines("./inputs/day9.txt");
-            List<int> results = new() {};
-            
-            foreach (var line in lines) {
-                var lineNumbers = line.Split(' ').Select(int.Parse).ToList();
-                List<List<int>> iterations = new() { lineNumbers };
-                while (true) {
-                    var differences = new List<int>() {};
-                    var onlySame = true;
-                    var lastIteration = iterations.Last();
-                    for (var i = 1; i < lastIteration.Count; i++) {
-                        var dif = lastIteration[i] - lastIteration[i - 1];
-                        if (lastIteration[i] != lastIteration[i - 1]) onlySame = false;
-                        differences.Add(dif);
-                    }
-                    iterations.Add(differences);
-                    if (onlySame) break;
-                }
-
-                iterations.Reverse();
-                results.Add(iterations.Aggregate(0, (acc, iteration) => iteration[0] - acc));
-
-            }
-            return results.Sum().ToString();
-        }
+        public string solve2() => File.ReadLines("./inputs/day9.txt").Select(line => 
+                calculate(line.Split(' ').Select(int.Parse).Reverse().ToList())
+            ).Sum().ToString();
     }
 }
