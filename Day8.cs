1a036db0dec4de9e28a65b1989da31fd81427514
Georgii Perepechko <g.perepechko@thesignalgroup.com>
2023-12-09 12:01:08 +0200

    day 8 part 2

--- Day8.cs
+++ Day8.cs
@@ -31,57 +31,40 @@ namespace AdventOfCode {
         }
 
         public string solve2() {
-            return "";
-            // var lines = File.ReadLines("./inputs/day8.txt");
-            // var i = -1;
-            // string instructions = "";
-            // Dictionary<string, MapNode> map = new() { };
-            // foreach (var line in lines) {
-            //     i++;
-            //     if (i == 0) {
-            //         instructions = line;
-            //     } else if (line.Length > 0) {
-            //         var parts = line.Split(" = ");
-            //         var exits = parts[1].Substring(1, parts[1].Length - 2).Split(", ");
-            //         map[parts[0]] = new MapNode(parts[0], exits[0], exits[1]);
-            //     }
-            // }
-
-            // var startingPoints = map.Keys.Where(k => k.EndsWith('A')).Select(k => map[k]);
-            // List<int> stepsToZ = new() { };
-            // foreach (var startingPoint in startingPoints) {
-            //     var current = startingPoint;
-            //     var stepsTaken = 0;
-            //     while (!current.name.EndsWith('Z')) {
-            //         var instruction = instructions[stepsTaken % instructions.Count()];
-            //         stepsTaken++;
-            //         current = map[instruction == 'L' ? current.left : current.right];
-            //     }
-            //     stepsToZ.Add(stepsTaken);
-            // }
-            // return FindLCM(stepsToZ).ToString();
-        }
-
-        private static int FindLCM(List<int> numbers) {
-            int result = numbers[0];
-            for (int i = 1; i < numbers.Count; i++) {
-                result = LCM(result, numbers[i]);
+            var lines = File.ReadLines("./inputs/day8.txt");
+            var i = -1;
+            string instructions = "";
+            Dictionary<string, MapNode> map = new() { };
+            foreach (var line in lines) {
+                i++;
+                if (i == 0) {
+                    instructions = line;
+                } else if (line.Length > 0) {
+                    var parts = line.Split(" = ");
+                    var exits = parts[1].Substring(1, parts[1].Length - 2).Split(", ");
+                    map[parts[0]] = new MapNode(parts[0], exits[0], exits[1]);
+                }
             }
 
-            return result;
-        }
-
-        private static int LCM(int a, int b) {
-            return Math.Abs(a * b) / GCD(a, b);
-        }
-
-        private static int GCD(int a, int b) {
-            while (b != 0) {
-                int temp = b;
-                b = a % b;
-                a = temp;
+            var startingPoints = map.Keys.Where(k => k.EndsWith('A')).Select(k => map[k]);
+            List<int> stepsToZ = new() { };
+            foreach (var startingPoint in startingPoints) {
+                var current = startingPoint;
+                var stepsTaken = 0;
+                while (!current.name.EndsWith('Z')) {
+                    var instruction = instructions[stepsTaken % instructions.Count()];
+                    stepsTaken++;
+                    current = map[instruction == 'L' ? current.left : current.right];
+                }
+                stepsToZ.Add(stepsTaken);
             }
+            return stepsToZ.Select(x => (decimal) x).Aggregate(LCM).ToString();
+        }
 
+        private static decimal LCM(decimal a, decimal b) => a * b / GCD(a, b);
+
+        private static decimal GCD(decimal a, decimal b) {
+            while (b != 0) (a, b) = (b, a % b);
             return a;
         }
     }
--- Program.cs
+++ Program.cs
@@ -4,13 +4,15 @@ namespace Main {
     static class Program {
         static void Main() {
             IDay[] days = {
-                new Day1(),
-                new Day2(),
-                new Day3(),
-                new Day4(),
-                new Day5(),
-                new Day6(),
-                new Day7(),
+                // new Day1(),
+                // new Day2(),
+                // new Day3(),
+                // new Day4(),
+                // new Day5(),
+                // new Day6(),
+                // new Day7(),
+                new Day8(),
+                // new Day9(),
             };
             Console.WriteLine($"Results:");
             var i = 0;
