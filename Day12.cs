86c13d1295dfe993aa7072352221377bef5fa65b
Georgii Perepechko <g.perepechko@thesignalgroup.com>
2023-12-12 18:50:35 +0200

    test input passing

--- Day12.cs
+++ Day12.cs
@@ -1,35 +1,36 @@
-using System.Text;
+using System.Numerics;
 
 namespace AdventOfCode {
     class Day12 : IDay {
         public string Solve1() {
-            return File.ReadLines("./inputs/day12-test.txt").Aggregate(0m, (sum, line) => {
+            return File.ReadLines("./inputs/day12-test.txt").Aggregate(new BigInteger(0), (sum, line) => {
                 var parts = line.Split(' ');
                 var (scheme, brokenRanges) = (parts[0], parts[1].Split(',').Select(n => int.Parse(n)).ToList());
                 var availableIntervals = scheme.Split('.', StringSplitOptions.RemoveEmptyEntries);
-                var possibleWaysCount = GetPossiblePlacementsCount(brokenRanges, availableIntervals, 0, 0, new Dictionary<string, decimal>());
+                var possibleWaysCount = GetPossiblePlacementsCount(brokenRanges, availableIntervals, 0, 0, new Dictionary<string, BigInteger>());
                 return sum + possibleWaysCount;
             }).ToString();
         }
 
         public string Solve2() {
             var i = 0;
-            return File.ReadLines("./inputs/day12-test.txt").Aggregate(0m, (sum, line) => {
+            return File.ReadLines("./inputs/day12-test.txt").Aggregate(new BigInteger(0), (sum, line) => {
                 var parts = line.Split(' ');
                 var (scheme, brokenRanges) = ($"{parts[0]}?{parts[0]}?{parts[0]}?{parts[0]}?{parts[0]}", $"{parts[1]},{parts[1]},{parts[1]},{parts[1]},{parts[1]}".Split(',').Select(n => int.Parse(n)).ToList());
                 var availableIntervals = scheme.Split('.', StringSplitOptions.RemoveEmptyEntries);
-                var possibleWaysCount = GetPossiblePlacementsCount(brokenRanges, availableIntervals, 0, 0, new Dictionary<string, decimal>());
+                var possibleWaysCount = GetPossiblePlacementsCount(brokenRanges, availableIntervals, 0, 0, new Dictionary<string, BigInteger>());
                 Console.WriteLine($"{++i}: {possibleWaysCount}");
                 return sum + possibleWaysCount;
             }).ToString();
         }
 
-        private decimal GetPossiblePlacementsCount(
+        private BigInteger GetPossiblePlacementsCount(
             List<int> rangesToFit,
             string[] availableIntervals,
             int rangesToFitIndex,
             int availableIntervalsIndex,
-            Dictionary<string, decimal> memo
+            Dictionary<string, BigInteger> memo
+            // string trail = ""
         ) {
             if (rangesToFitIndex >= rangesToFit.Count) return availableIntervals.Skip(availableIntervalsIndex).Any(i =>i.Contains('#')) ? 0 : 1;
             if (availableIntervalsIndex >= availableIntervals.Length) return 0;
@@ -39,19 +40,17 @@ namespace AdventOfCode {
             var memoKey = $"{rangesToFitIndex}/{availableIntervalsIndex}/{intervalToUse}";
             if (memo.ContainsKey(memoKey)) return memo[memoKey];
 
-            var resultOfSkippingInterval = GetPossiblePlacementsCount(rangesToFit, availableIntervals, rangesToFitIndex, availableIntervalsIndex + 1, memo);
+            var resultOfSkippingInterval = GetPossiblePlacementsCount(rangesToFit, availableIntervals, rangesToFitIndex, availableIntervalsIndex + 1, memo); // , trail + $"-unused({availableIntervalsIndex})-");
             var maxWaysToFitCount = intervalToUse.Length - rangeToSubtract + 1;
-            var resultsOfUsingInterval = 0m;
+            var resultsOfUsingInterval = new BigInteger(0);
             for (var offset = 0; offset < maxWaysToFitCount; offset++) {
                 var dividerIndex = rangeToSubtract + offset;
-                if (
-                    (offset > 0 && intervalToUse[offset - 1] == '#') ||
-                    (dividerIndex < intervalToUse.Length && intervalToUse[dividerIndex] == '#')
-                ) continue; // cannot start right after or end right before "#"
+                if (dividerIndex < intervalToUse.Length && intervalToUse[dividerIndex] == '#') continue;
+                if (offset > 0 && intervalToUse[offset - 1] == '#') break;
                 var intervalsAfterRangeFit = (string[])availableIntervals.Clone();
                 var updatedInterval = intervalToUse.Length > dividerIndex + 1 ? intervalToUse.Substring(dividerIndex + 1) : "";
                 intervalsAfterRangeFit[availableIntervalsIndex] = updatedInterval;
-                resultsOfUsingInterval += GetPossiblePlacementsCount(rangesToFit, intervalsAfterRangeFit, rangesToFitIndex + 1, availableIntervalsIndex, memo);
+                resultsOfUsingInterval += GetPossiblePlacementsCount(rangesToFit, intervalsAfterRangeFit, rangesToFitIndex + 1, availableIntervalsIndex, memo); // , trail + $"-used({intervalToUse} for {rangeToSubtract} with {offset})-");
             }
             memo[memoKey] = resultOfSkippingInterval + resultsOfUsingInterval;
             return resultOfSkippingInterval + resultsOfUsingInterval;
